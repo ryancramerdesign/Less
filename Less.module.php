16b10bf7383b591b1744ee042d9d4e09d1a3d3f1
Ryan Cramer <ryan@processwire.com>
2021-05-25 15:47:53 -0400

    Initial commit
    Co-authored-by: BernhardBaumrock <office@baumrock.com>

---
+++ Less.module.php
@@ -0,0 +1,195 @@
+<?php namespace ProcessWire;
+
+/**
+ * LESS parser module for ProcessWire
+ * 
+ * Usage
+ * ~~~~~
+ * // compile 2 less files to a css file
+ * $less = $modules->get('Less');
+ * $less->setOption('compress', true);
+ * $less->addFile('/path/to/file1.less');
+ * $less->addFile('/path/to/file2.less'); 
+ * $less->saveCss('/path/to/file.min.css'); 
+ * 
+ * // access wikimedia less parser directly (it has many methods)
+ * $parser = $less->parser();
+ * $parser->parseFile('/path/to/file.less');
+ * $css = $parser->getCss();
+ * ~~~~~
+ * 
+ */
+
+class Less extends WireData implements Module {
+
+	public static function getModuleInfo() {
+		return array(
+			'title' => 'Less',
+			'version' => 1,
+			'summary' => 'Less Parser for ProcessWire',
+			'author' => 'Bernhard Baumrock, Ryan Cramer',
+			'icon' => 'css3',
+			'singular' => false,
+			'requires' => 'ProcessWire>=3.0.164',
+		);
+	}
+
+	/**
+	 * @var \Less_Parser|null
+	 * 
+	 */
+	protected $parser = null;
+
+	/**
+	 * @var array
+	 * 
+	 */
+	protected $options = array(
+		'compress' => false,
+	);
+
+	/**
+	 * Construct
+	 * 
+	 */
+	public function __construct() {
+		$this->set('compress', false);
+		parent::__construct();
+	}
+
+	/**
+	 * Init
+	 * 
+	 */
+	public function init() { }
+
+	/**
+	 * Set one or more options from associative array
+	 * 
+	 * @param array $options
+	 * @param bool $reset Remove previously set options rather than merging them?
+	 * @return $this
+	 * 
+	 */
+	public function setOptions(array $options, $reset = false) {
+		$this->options = $reset ? $options : array_merge($this->options, $options);
+		return $this;
+	}
+
+	/**
+	 * Set one option
+	 * 
+	 * @param string $name
+	 * @param mixed $value
+	 * @return $this
+	 * 
+	 */
+	public function setOption($name, $value) {
+		$this->options[$name] = $value;
+		return $this;
+	}
+
+	/**
+	 * Get currently set options
+	 * 
+	 * @return array
+	 * 
+	 */
+	public function getOptions() {
+		return $this->options;
+	}
+	
+	/**
+	 * Get instance of less parser
+	 * 
+	 * This returns the same instance every time unless you $reset
+	 * 
+	 * @param bool $reset
+	 * @return \Less_Parser
+	 * 
+	 */
+	public function parser($reset = false) {
+		if(is_array($reset)) { 
+			// options specified with parser call, undocumented, for compatibility with Bernhard's version
+			$this->setOptions($reset); 
+			$reset = false;
+		}
+		if(!$reset && $this->parser) return $this->parser;
+		if(!class_exists('\Less_Parser', false)) {
+			require_once(__DIR__ . '/wikimedia/less.php/lib/Less/Autoloader.php');
+			\Less_Autoloader::register();
+		}
+		$this->parser = new \Less_Parser($this->options);
+		return $this->parser;
+	}
+
+	/**
+	 * Add a LESS file to parse 
+	 * 
+	 * @param sring $file
+	 * @return self
+	 * @throws \Less_Exception_Parser
+	 * 
+	 */
+	public function parseFile($file) {
+		$this->parser()->parsefile($file);
+		return $this;
+	}
+
+	/**
+	 * Add a LESS file to parse (alias of parseFile method)
+	 * 
+	 * @param string $file
+	 * @return self
+	 * @throws \Less_Exception_Parser
+	 * 
+	 */
+	public function addFile($file) {
+		return $this->parseFile($file);
+	}
+
+	/**
+	 * Get compiled CSS
+	 * 
+	 * @return string
+	 * @throws \Exception
+	 * 
+	 */
+	public function getCss() {
+		return $this->parser()->getCss();
+	}
+
+	/**
+	 * Save to CSS file
+	 * 
+	 * @param string $file
+	 * @param string|null $css CSS to save or omit to save CSS compiled from added .less files
+	 * @return array
+	 * @throws WireException
+	 * 
+	 */
+	public function saveCss($file, $css = null) {
+		$files = $this->wire()->files;
+		$file = $files->unixFileName($file);
+		if(empty($css)) $css = $this->parser()->getCss();
+		if(empty($css)) return false;
+		return $files->filePutContents($file, $css);
+	}
+
+	/**
+	 * Returns LESS parser compatible with lessphp API (https://github.com/leafo/lessphp)
+	 * 
+	 * Note: this returns a new instance on every call. Whatever you do with lessc is 
+	 * separate from the methods in this module, so if using lessc you should only use the
+	 * returned lessc instance and not the API of this module. 
+	 * 
+	 * @return \lessc
+	 * 
+	 */
+	public function lessc() {
+		if(!class_exists('\lessc')) {
+			require_once(__DIR__ . '/wikimedia/less.php/lessc.inc.php'); 
+		}
+		return new \lessc();
+	}
+}
